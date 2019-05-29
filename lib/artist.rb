1d0ebb1b90cac408d830433b6149a4ffca201633
Travis Exline <t.exline22@gmail.com>
2019-05-29 14:13:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,21 @@
 class Artist
+
   attr_accessor :name, :songs
+
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-  end
-
-  def save
-    @@all << self
+    #this does not follow the SSOT principle
   end
 
   def self.all
     @@all
+  end
+
+  def save
+    @@all << self
   end
 
   def add_song(song)
@@ -20,12 +23,12 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name || self.new(name).save}
-  end
-
-  def print_songs
-    @songs.each do |song|
-      puts song.name
+    found_arist = @@all.find{ |artist| artist.name == name}
+    if found_arist == nil
+      self.new(name)
+    else
+      found_arist
     end
   end
+
 end
