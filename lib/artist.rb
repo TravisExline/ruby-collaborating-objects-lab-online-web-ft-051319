6814553e1dd9372579f0386a2654e8e6bd3cac46
Travis Exline <t.exline22@gmail.com>
2019-05-29 14:17:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,9 +28,9 @@ class Artist
     end
   end
 
-  #def print_songs
-  #  @songs.each do |song|
-    #  puts song.name
-  #  end
-  #end
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
 end
